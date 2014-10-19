b0f77bb7085773c1162eb5d7ae0d1fd723af0c4a
Jiří Pinkava <j-pi@seznam.cz>
2014-10-19 21:49:25 +0200

    parametrize multi reciever

--- radio-tetra/tetra_rx_multi.py
+++ radio-tetra/tetra_rx_multi.py
@@ -22,29 +22,38 @@ class tetra_rx_multi(gr.top_block):
     def __init__(self):
         gr.top_block.__init__(self, "Tetra Rx Multi")
 
+        options = self.get_options()
+
         ##################################################
         # Variables
         ##################################################
-        self.srate_rx = srate_rx = 1800000
-        self.channels = 36*2
+        self.srate_rx = srate_rx = options.sample_rate
+        self.channels = srate_rx / 25000
 
         ##################################################
         # Blocks
         ##################################################
         self.rtlsdr_source = osmosdr.source( args="numchan=" + str(1) + " " + "" )
         self.rtlsdr_source.set_sample_rate(srate_rx)
-        self.rtlsdr_source.set_center_freq(424e6, 0)
-        self.rtlsdr_source.set_freq_corr(36, 0)
+        self.rtlsdr_source.set_center_freq(options.frequency, 0)
+        self.rtlsdr_source.set_freq_corr(options.ppm, 0)
         self.rtlsdr_source.set_dc_offset_mode(0, 0)
         self.rtlsdr_source.set_iq_balance_mode(0, 0)
-        self.rtlsdr_source.set_gain_mode(False, 0)
-        self.rtlsdr_source.set_gain(36, 0)
+        if options.gain is not None:
+            self.rtlsdr_source.set_gain_mode(False, 0)
+            self.rtlsdr_source.set_gain(36, 0)
+        else:
+            self.rtlsdr_source.set_gain_mode(True, 0)
+
+        out_type, dst_path = options.output.split("://", 1)
+        if out_type == "udp":
+            dst_ip, dst_port = dst_path.split(':', 1)
 
         self.pfb_channelizer_ccf = pfb.channelizer_ccf(
-        	  self.channels,
-        	  (firdes.root_raised_cosine(1, srate_rx, 18000, 0.35, 1024)),
-        	  36./25.,
-        	  100)
+              self.channels,
+              (firdes.root_raised_cosine(1, srate_rx, 18000, 0.35, 1024)),
+              36./25.,
+              100)
         #self.pfb_channelizer_ccf.set_channel_map(([]))
         #self.pfb_channelizer_ccf.declare_sample_delay(0)
 
@@ -53,7 +62,14 @@ class tetra_rx_multi(gr.top_block):
         for ch in range(0, self.channels):
             mpsk = digital.mpsk_receiver_cc(
                     4, cmath.pi/4, cmath.pi/100.0, -0.5, 0.5, 0.25, 0.001, 2, 0.001, 0.001)
-            sink = blocks.udp_sink(gr.sizeof_float*2, "", 60001+ch, 1472, True)
+            if out_type == 'udp':
+                sink = blocks.udp_sink(gr.sizeof_gr_complex, dst_ip, int(dst_port)+ch, 1472, True)
+            elif out_type == 'file':
+                print dst_path % ch
+                sink = blocks.file_sink(gr.sizeof_gr_complex*1, dst_path % ch, False)
+                sink.set_unbuffered(True)
+            else:
+                raise ValueError("Invalid output URL '%s'" % options.output)
 
             self.connect((self.pfb_channelizer_ccf, ch),
                     (mpsk, 0),
@@ -75,9 +91,34 @@ class tetra_rx_multi(gr.top_block):
         self.rtlsdr_source.set_sample_rate(self.srate_rx)
         self.pfb_channelizer_ccf.set_taps((firdes.root_raised_cosine(1, self.srate_rx, 18000, 0.35, 1024)))
 
+    def get_options(self):
+        parser = OptionParser(option_class=eng_option)
+
+        parser.add_option("-a", "--args", type="string", default="",
+                help="gr-osmosdr device arguments")
+        parser.add_option("-s", "--sample-rate", type="eng_float", default=1800000,
+                help="set receiver sample rate (default 1800000, must be multiple of 900000)")
+        parser.add_option("-f", "--frequency", type="eng_float", default=394.4e6,
+                help="set receiver center frequency")
+        parser.add_option("-p", "--ppm", type="eng_float", default=0.,
+                help="Frequency correction as PPM")
+        parser.add_option("-g", "--gain", type="eng_float", default=None,
+                help="set receiver gain")
+        parser.add_option("-o", "--output", type=str,
+                help="output URL (eg file:///<FILE_PATH> or udp://DST_IP:PORT, use %d for channel no.")
+
+        (options, args) = parser.parse_args()
+        if len(args) != 0:
+            parser.print_help()
+            raise SystemExit, 1
+        options.sample_rate = int(options.sample_rate)
+        if options.sample_rate % 900000:
+            parser.print_help()
+            raise SystemExit, 1
+        return (options)
+
+
 if __name__ == '__main__':
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-    (options, args) = parser.parse_args()
     tb = tetra_rx_multi()
     tb.start()
     tb.wait()
