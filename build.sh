ae3f9631a62eb04417730f764297cc00e51e67fc
Jenda <jenda.jzqk73@hrach.eu>
2014-11-07 00:38:37 +0100

    check that downloaded codec source matches

--- build.sh
+++ build.sh
@@ -8,10 +8,18 @@ cd $ROOT
 mkdir -p codec
 cd $ROOT/codec
 
+CHASH=1fe18c4773c8ccb52ef23ca5b4a0b0841b38d54ddd5d9c7d86eb8b060f132f39
+
 # download audio codec, if needed
-echo "1fe18c4773c8ccb52ef23ca5b4a0b0841b38d54ddd5d9c7d86eb8b060f132f39  en_30039502v010301p0.zip" | sha256sum -c - || (
+echo "$CHASH  en_30039502v010301p0.zip" | sha256sum -c - || (
     rm -f en_30039502v010301p0.zip
     wget http://www.etsi.org/deliver/etsi_en/300300_300399/30039502/01.03.01_60/en_30039502v010301p0.zip
+    )
+
+# check that the downloaded file is OK
+echo "$CHASH  en_30039502v010301p0.zip" | sha256sum -c - || (
+    echo "Downloaded codec does not match checksum $CHASH, will not continue!"
+    exit 1
     )
 
 # build audio codec
