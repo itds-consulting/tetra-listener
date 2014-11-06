d292600039ada6c02301c97b71b334e68e621486
Jiří Pinkava <j-pi@seznam.cz>
2014-11-07 00:40:42 +0100

    cleanup config

--- radio-tetra/config.sh
+++ radio-tetra/config.sh
@@ -2,13 +2,12 @@
 
 # TETRA listener configuration file
 
-
 [ -d "$ROOT" ] || (echo "Invalid use of config.sh ROOT must be set by parent script"; exit 1)
 
 # path to OSMO-TETRA reciever
 OSMOTETRA_DIR=${ROOT}/osmo-tetra/src
 # path to root directory of audio codec binaries
-CODEC_DIR=$ROOT/codec/c-code
+CODEC_DIR=${ROOT}/codec/c-code
 
 # used by monitor (must by run in crontab, see tetra-monitor.sh for details)
 MONITOR_LOCK=/tmp/brmtetra-moitor.lock
@@ -31,27 +30,25 @@ REC_FORMAT=ogg
 STREAMS=72
 
 TUNE_FREQ=424e6
-TUNE_PPM=55
+TUNE_PPM=0
 # set gain, comment out for automatic gain setting
 #TUNE_GAIN=36
 # band width usefull for automatic squelch threshold detection
 TUNE_ATD_BW=1400000
 # therhold (above detected noise level) fot automatic squelch
-TUNE_ATD_LEVEL=3
+TUNE_ATD_LEVEL=4
 # arguments for osmo-sdr RX block, use rtl_tcp=<ADDR>:<PORT> for TCP source
 TUNE_OSMO_ARGS=""
 # automatically tune PPM to selected channel
-AUTO_TUNE_CHANNEL=70
+#AUTO_TUNE_CHANNEL=0
 
 # set to yes to enable debug logging
-DEBUG=yes
+#DEBUG=yes
 # comma separated list of channels for printing signal strenght
-DEBUG_CHENNELS_PWR="0,1,2,3,4,5,6,71,70,69,68,67,66"
+DEBUG_CHENNELS_PWR=""
 
 # graaaah, workarounds
 
-LD_LIBRARY_PATH=$ROOT/
-PYTHONPATH=$PYTHONPATH:$ROOT/osmo-tetra/src/demod
+LD_LIBRARY_PATH=${ROOT}/
+PYTHONPATH=${PYTHONPATH}:${ROOT}/osmo-tetra/src/demod
 
-#LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib
-#PYTHONPATH=/usr/local/lib/python2.7/site-packages:/usr/local/lib/python2.7/dist-packages:/usr/local/lib64/python2.7/site-packages:/usr/local/lib64/python2.7
